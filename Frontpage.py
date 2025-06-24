cc32a49fa5646049ba3ff5a20c1b6d82386c4086
Valerio Botto <valeriobotto@outlook.it>
2025-06-24 17:10:05 +0200

    Update landing page and fix Streamlit Cloud issue

--- Frontpage.py
+++ Frontpage.py
@@ -23,12 +23,12 @@ st.write(
 st.markdown("---")
 
 #dataset download section
-labels_and_icons = {
-    "dim_geography.csv": ("Geography dimension", ":material/public:"),
-    "dim_symbol.csv": ("Symbol dimension", ":material/sell:"),
-    "dim_time.csv": ("Time dimension", ":material/event:"),
-    "dim_transaction_type.csv": ("Transaction Type", ":material/receipt_long:"),
-    "fact_transactions.csv": ("Fact transactions", ":material/attach_money:")
+labels = {
+    "dim_geography.csv": "Geography dimension",
+    "dim_symbol.csv": "Symbol dimension",
+    "dim_time.csv": "Time dimension",
+    "dim_transaction_type.csv": "Transaction Type",
+    "fact_transactions.csv": "Fact transactions"
 }
 
 st.subheader("Download Dataset Files ⬇️")
@@ -40,16 +40,13 @@ csv_files = [f for f in os.listdir(data_dir) if f.endswith(".csv")]
 for i in range(0, len(csv_files), 3):
     cols = st.columns(3)
     for col, filename in zip(cols, csv_files[i:i+3]):
-        label, icon = labels_and_icons.get(
-            filename, (f"Download {filename}", "📂")
-        )
+        label = labels.get(filename, f"Download {filename}")
         with open(os.path.join(data_dir, filename), "rb") as f:
             col.download_button(
                 label=label,
                 data=f,
                 file_name=filename,
                 mime="text/csv",
-                icon=icon,
                 use_container_width=True
             )
 
@@ -60,7 +57,6 @@ with col1:
     st.link_button(
         label="LinkedIn",
         url="https://www.linkedin.com/in/valerio-botto-4844b2190/",
-        icon=":material/assignment_ind:",
         use_container_width=True
     )
 
@@ -68,10 +64,8 @@ with col2:
     st.link_button(
         label="GitHub",
         url="https://github.com/ValerioBotto",
-        icon=":material/code:",
         use_container_width=True
     )
-
 
 st.write("\n")
 
